e6f7aacb1682fc15c402ec22c11426f355cccc8d
Man Huynh <man.huynh@mgm-tp.com>
2025-06-29 13:08:00 +0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',      // 👈 chạy từ container
-    port: 5173,           // 👈 port cần expose
-    strictPort: true,     // 👈 ép chạy đúng port, không đổi nếu bị chiếm
-    allowedHosts: ["localhost", "127.0.0.1", "0.0.0.0", "ai-agent.hykura.id.vn"]
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    allowedHosts: ["localhost", "127.0.0.1", "0.0.0.0", "ai-agent.hykura.id.vn"],
+    
+    // 👇 thêm headers cho iframe
+    headers: {
+      'X-Frame-Options': 'ALLOWALL',
+      'Content-Security-Policy': "frame-ancestors *"
+    }
   }
-  
 })
